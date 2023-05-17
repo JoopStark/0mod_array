b1b71b5ccc6fdd6c3158d53d31b969ca1e4c7c25
Joop Stark <stark.joop@gmail.com>
2023-05-17 11:53:42 -0600

    Call arrays

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,16 @@
-summit_towns = %w{Silverthorne Breckenridge Frisco Dillon}
-even_num = {2, 3, 4, 6 }
-irrational = {3.14, 1.61, 2.72, 0.333}
-stocked = {true, false, true, true}
+summit_towns = %w[Silverthorne Breckenridge Frisco Dillon]
+even_num = [2, 3, 4, 6]
+irrational = [3.14, 1.61, 2.72, 0.333]
+stocked = [true, false, true, true]
+
+puts summit_towns[1]
+# Breckenridge
+
+puts even_num[0]
+# 2
+
+puts irrational[-1]
+# 0.333
+
+puts stocked[2]
+# true
